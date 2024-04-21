7bd656458440549cd0597dc14fe144a9e2c3c17c
Leonardo Silva <81488877+leonardoprogrammer@users.noreply.github.com>
2024-04-21 10:51:20 -0300

    Exercício Módulo 12

---
+++ mod12/ExercicioMod12/src/br/com/leonardosilva/Main.java
@@ -0,0 +1,56 @@
+package br.com.leonardosilva;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String nomes;
+
+        System.out.println("Digite nomes separados por vírgulo junto com gênero separado por traço:");
+
+        // RECEBE OS NOMES COM SCANNER
+        nomes = scan.nextLine();
+
+        // SEPARA OS NOMES POR VÍRGULA
+        List<String> nomesList = separaNomesPorVirgula(nomes);
+
+        Set<String> grupoMasculino = new LinkedHashSet<>();
+        Set<String> grupoFeminino = new LinkedHashSet<>();
+
+        for (int i = 0; i < nomesList.size(); i++) {
+            String[] splitTrace = nomesList.get(i).split("-");
+            if (splitTrace[1].trim().equalsIgnoreCase("M")) {
+                // SE O CARACTER FOR "M" ADICIONA NOME AO GRUPO MASCULINO
+                grupoMasculino.add(splitTrace[0].trim());
+            } else if (splitTrace[1].trim().equalsIgnoreCase("F")) {
+                // SE O CARACATER FOR "F" ADICONA NOME AO GRUPO FEMININO
+                grupoFeminino.add(splitTrace[0].trim());
+            }
+        }
+
+        // IMPRIME OS NOMES DO GRUPO MASCULINO
+        System.out.println("NOMES DO GRUPO MASCULINO:");
+        System.out.println(grupoMasculino);
+
+        // IMPRIME OS NOMES DO GRUPO FEMININO
+        System.out.println("\nNOMES DO GRUPO FEMININO:");
+        System.out.println(grupoFeminino);
+    }
+
+    /**
+     * Separa os nomes por vírgula e armazena em uma lista
+     * @param nomes
+     * @return java.util.List
+     */
+    public static List<String> separaNomesPorVirgula(String nomes) {
+        List<String> nomesList = Arrays.asList(nomes.split(","));
+
+        for (int i = 0; i < nomesList.size(); i++) {
+            nomesList.set(i, nomesList.get(i).trim());
+        }
+
+        return nomesList;
+    }
+}
